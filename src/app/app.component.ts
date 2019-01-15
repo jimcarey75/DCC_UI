f01063667c49d05b400a4b4315b001942331ecdd
Jim Carey <Jim.Carey@landmark.co.uk>
2019-01-15 14:40:17 +0000

    added success message on 100% completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   progressTitle = 'Switch Request Received';
-  progressValue = 50;
-  value = 50;
+  progressValue = 10;
+  showMessage: boolean = false;
+  
+  array = [ 
+    {
+      progressTitle: 'status 0',
+      progressStatus: 0
+    }, 
+    {
+      progressTitle: 'status 10',
+      progressStatus: 10
+    }, 
+    {
+      progressTitle: 'status 20',
+      progressStatus: 20
+    }, 
+    {
+      progressTitle: 'status 30',
+      progressStatus: 30
+    }, 
+    {
+      progressTitle: 'status 40',
+      progressStatus: 40
+    }, 
+    {
+      progressTitle: 'status 50',
+      progressStatus: 50
+    }, 
+    {
+      progressTitle: 'status 60',
+      progressStatus: 60
+    }, 
+    {
+      progressTitle: 'status 70',
+      progressStatus: 70
+    }, 
+    {
+      progressTitle: 'status 80',
+      progressStatus: 80
+    }, 
+    {
+      progressTitle: 'status 90',
+      progressStatus: 90
+    }, 
+    {
+      progressTitle: 'status 100',
+      progressStatus: 100
+    }
 
-  public setValue(value: number): void {
+  ];
 
-    this.value = value;
 
+
+  setValue(progressValue: number, progressTitle: string): void {
+
+    this.progressValue = 50;
+    this.progressTitle = "Status has changed";
+  }
+
+  checkValue(progressValue: number){
+    if (this.progressValue == 100) {
+      this.showMessage = true;      
+    }
+    else {
+      this.showMessage = false;
+    }
   }
 }
